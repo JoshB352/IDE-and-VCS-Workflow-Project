063f6ceb3c70193249d1c0907d21efd251c7152d
divin <divin@JESUS-IS-KING>
2025-06-13 19:05:28 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project.cpp
@@ -0,0 +1,13 @@
+// IDE and VCS Workflow Project.cpp
+// Program displays your initials
+
+#include <iostream>
+using namespace std;
+int main() {
+    char first, middle, last;
+    first = 'J';
+    middle = 'A';
+    last = 'B';
+    cout << first << middle << last << endl;
+    return 0;
+}
