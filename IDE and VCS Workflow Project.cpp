02720c4e3d71dc301d026b7830f765278db39d34
Josh Budd <jab35211@email.vccs.edu>
2025-07-22 17:00:54 -0400

    Finished program.

--- IDE and VCS Workflow Project.cpp
+++ IDE and VCS Workflow Project.cpp
@@ -1,50 +1,77 @@
-// Program predicts the amount of organisms in a populations based on the numbers givven
+// This program gets the length and width of a rectangle, then calculates and displays its perimeter and area
 
 #include <iostream>
 using namespace std;
+
+void getLength_Width(double& length, double& width);\
+double calcPerimeter(double length, double width);
+double calcArea(double length, double width);
+void displayProperties(double perimeter, double area);
+
 int main()
 {
-	int populationStart;
-	double dailyPercent;
-	int numberOfDays;
+	char askToContinue;
+	double perimeter, area;
+	double length, width;
 
-	// Collect numbers from user.
-	cout << "This program predicts the size of a population of organisms." << endl;
-	cout << "What is the starting number of organism? (Note that the starting size cannot be less than 2): ";
-	cin >> populationStart;
-	while (populationStart < 2)
-	{
-		cout << "\nInvalid input. Please enter a number that is greater than or equal to 2: ";
-		cin >> populationStart;
-	}
-	cin.ignore();
+	 do {
+        getLength_Width(length, width);
+        perimeter = calcPerimeter(length, width);
+        area = calcArea(length, width);
+        displayProperties(perimeter, area);
 
-	cout << "\nWhat is the average daily population increase (Note that a negative number will not be accepted by the program): ";
-	cin >> dailyPercent;
-	while (dailyPercent < 0)
-	{
-		cout << "\nInvalid input. Please enter a number that is not negative: ";
-		cin >> dailyPercent;
-	}
-	cin.ignore();
+        cout << "\nDo you want continue the program? Type 'y' for YES, or 'n' for NO: ";
+        cin >> askToContinue;
+        askToContinue = toupper(askToContinue);
+    } while (askToContinue == 'Y');
 
-	cout << "\nWhat is the number of days they will multiply for (Note that the number of days cannot be less than 1): ";
-	cin >> numberOfDays;
-	while (numberOfDays < 1)
-	{
-		cout << "\nInvalid input. Please enter a number that is greater than or equal to 1: ";
-		cin >> numberOfDays;
-	}
-
-	// Calculate and display numbers.
-	cout << "\nDay\tPopulation\n";
-	cout << "____________________\n";
-	int population = populationStart;
-
-	for (int day = 1; day <= numberOfDays; ++day)
-	{
-		cout << day << "\t" << population << endl;
-		population += (population * (dailyPercent / 100));
-	}
-	return 0;
+    cout << "Terminat(or)ed..." << endl;
+    return 0;
 }
+
+/*
+ This function prompts the user to enter the length and width of a rectangle and validates the input. It returns both quantities back to the calling function. Validation – length and width must be > 0.
+(Note – Since this function returns two values, the parameters must be passed by reference).
+ */
+void getLength_Width(double& length, double& width) {
+    do {
+        cout << "Enter the length of the rectangle: ";
+        cin >> length;
+        if (length <= 0)
+            cout << "Invalid input. Please enter a length that is positive and greater than 0.\n";
+    } while (length <= 0);
+
+    do {
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        if (width <= 0)
+            cout << "Invalid input. Please enter a width that is positive and greater than 0.\n";
+    } while (width <= 0);
+}
+
+/*
+ This function takes as input the length and width of a rectangle, calculates the perimeter, and returns the result of the calculation to the caller.
+(Note – This function assumes that the length and width are provided and that the values have been validated. This function returns the perimeter to the calling function which then decides what to do with the value)
+ */
+double calcPerimeter(double length, double width) {
+    return 2 * (length + width);
+}
+
+/*
+ This function takes as input the length and width of a rectangle, calculates the area and returns the area to the caller.
+(Note – This function assumes that the length and width are provided and that the values have been validated. This function returns the perimeter to the calling function which then decides what to do with the value.)
+ */
+double calcArea(double length, double width) {
+    return length * width;
+}
+
+/*
+ This function takes as input the perimeter and area of a rectangle and displays these values on the monitor.
+(Note – this function does not return anything to the caller, so the postcondition is that it writes the values to the display).
+ */
+void displayProperties(double perimeter, double area) {
+    cout << "\nRectangle Properties:";
+    cout << "\nPerimeter: " << perimeter;
+    cout << "\nArea: " << area << endl;
+}
+
