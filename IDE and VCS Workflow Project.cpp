8e61d2657deaef186d339e5c70b0a3ba60999814
Josh Budd <jab35211@email.vccs.edu>
2025-07-22 20:20:28 -0400

    Finished the program.

--- IDE and VCS Workflow Project.cpp
+++ IDE and VCS Workflow Project.cpp
@@ -1,31 +1,71 @@
-/*
- This program produces a table showing degrees Fahrenheit between the values of 0 and 20 with the corresponding Celsius value.
- The table has a title, the columns are labeled, and the temperature values are right aligned in each column. 
- Temperature values are displayed with one decimal point of precision.
-*/
+// This program calculates a performer’s score. Five judges each contribute a score, the highest and lowest score is dropped and the remaining three are averaged
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-const int minimumFahrenheit = 0;
-const int maximumFahrenheit = 20;
-
-// This takes as input a temperature value in degrees Fahrenheit and returns the equivalent value of the temperature in degrees Celsius.
-double getCelsius(int fahrenheit) {
-    return (5.0 / 9) * (fahrenheit - 32);
-}
+double getScore();
+bool isLower(double a, double b);
+bool isHigher(double a, double b);
+double calcAverage(double total, double minimumScore, double maximumScore);
 
 int main() {
-    cout << "Fahrenheit Converted to Celsius (0f to 20f)\n";
-    cout << setw(12) << "Fahrenheit" << setw(12) << "Celsius" << endl;
-    cout << "---------------------------\n";
+    const int amountOfJudges = 5;
+    double score, total = 0;
+    double minimumScore, maximumScore, averageScore;
 
-    for (int fahrenheit = minimumFahrenheit; fahrenheit <= maximumFahrenheit; ++fahrenheit) 
+    score = getScore();
+    minimumScore = maximumScore = score;
+    total += score;
+
+    for (int i = 1; i < amountOfJudges; ++i)
     {
-        double celsius = getCelsius(fahrenheit);
-        cout << setw(12) << fahrenheit << setw(12) << fixed << setprecision(1) << celsius << endl;
+        score = getScore();
+        total += score;
+
+        if (isLower(score, minimumScore))
+            minimumScore = score;
+        if (isHigher(score, maximumScore))
+            maximumScore = score;
     }
 
+    averageScore = calcAverage(total, minimumScore, maximumScore);
+
+    cout << fixed << setprecision(2);
+    cout << "\nFinal average score (after excluding the highest and lowest scores): " << averageScore << endl;
+
     return 0;
+}
+
+// This functions asks the user to enter one of the five judge's scores, returnging a score within the range of 0 to 10 based on the users input.
+
+double getScore() {
+    double score;
+    do 
+    {
+        cout << "Enter the judge's given score. Scores range from 0 to 10: ";
+        cin >> score;
+        if (score < 0.0 || score > 10.0)
+            cout << "Invalid input, the score must be between 0 and 10.\n";
+    } while (score < 0.0 || score > 10.0);
+    return score;
+}
+
+// This function takes two integer values as input, it returns true if the first value is <= the second value, otherwise it returns false.
+
+bool isLower(double a, double b) {
+    return a <= b;
+}
+
+// This function takes two integer values as input, it returns true if the first value is >= the second value, otherwise it returns false.
+bool isHigher(double a, double b) {
+    return a >= b;
+}
+
+/*
+ This function takes as input the total of all the scores, the number of scores, the minimum score value and the maximum score value.
+ It returns the average of the three middle scores by subtracting out the minimum and maximum scores from the total.
+ */
+double calcAverage(double total, double minimumScore, double maximumScore) {
+    return (total - minimumScore - maximumScore) / 3.0;
 }
